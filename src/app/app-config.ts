fab5711c63bbb1c832a07b8abc56242e06235698
Chris Ha <chunghha@users.noreply.github.com>
2017-07-12 17:22:09 -0500

    Chore(Compodoc): Routes parsing error

---
+++ src/app/app-config.ts
@@ -0,0 +1,17 @@
+export const APP_CONFIG = {
+    path: {
+        empty: '',
+        hash: '#',
+        match: 'full',
+        redirect: '/elephants'
+    },
+    elephant: {
+        path: 'elephants'
+    },
+    feedback: {
+        path: 'feedback',
+    },
+    lion: {
+        path: 'lions'
+    }
+}
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,10 +1,14 @@
+import { Routes } from '@angular/router'
+
 import { ElephantPageComponent } from './elephants/page';
 import { LionPageComponent } from './lions/page';
 import { FeedbackFormComponent } from './feedback/page';
 
-export const appRoutes = [
-  { path: '', redirectTo: '/elephants', pathMatch: 'full' },
-  { path: 'elephants', component: ElephantPageComponent },
-  { path: 'lions', component: LionPageComponent },
-  { path: 'feedback', component: FeedbackFormComponent },
+import { APP_CONFIG } from './app-config';
+
+export const appRoutes: Routes = [
+  { path: APP_CONFIG.path.empty, redirectTo: APP_CONFIG.path.redirect, pathMatch: APP_CONFIG.path.match },
+  { path: APP_CONFIG.elephant.path, component: ElephantPageComponent },
+  { path: APP_CONFIG.lion.path, component: LionPageComponent },
+  { path: APP_CONFIG.feedback.path, component: FeedbackFormComponent },
 ];
